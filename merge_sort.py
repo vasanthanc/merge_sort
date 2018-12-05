2ef98ccf8da492e7dc5c3bdf240d98d522e118e9
Vasanthan Chandrasekaran <vasanthan93c@gmail.com>
2018-12-06 00:01:04 +0530

    merge sort algorithm
    design: here I have implemented a merge sort algorithm
    in class-based design.

--- merge_sort.py
+++ merge_sort.py
@@ -1,16 +1,23 @@
 INITIAL = 0
-def sorting(arr):
-    arr_length = len(arr)
-    if arr_length > 1:
-        mid = arr_length//2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-        deep_left = sorting(left_half)
-        deep_right = sorting(right_half)
 
+class MergeSort:
+    def __init__(self):
+        pass
+
+    def sorting(self,unsorted_array):
+        arr_length = len(unsorted_array)
+        if arr_length > 1:
+            mid = arr_length//2
+            left_half = unsorted_array[:mid]
+            right_half = unsorted_array[mid:]
+            deep_left = self.sorting(left_half)
+            deep_right = self.sorting(right_half)
+            return self.merge(deep_left, deep_right, unsorted_array)
+        return unsorted_array
+
+    def merge(self, deep_left, deep_right, arr):
         left_counter = right_counter = arr_counter = INITIAL
-
-        while left_counter < len(deep_left) and right_counter < len(deep_right):
+        while left_counter < len (deep_left) and right_counter < len (deep_right):
             if deep_left[left_counter] > deep_right[right_counter]:
                 arr[arr_counter] = deep_right[right_counter]
                 right_counter += 1
@@ -19,18 +26,21 @@ def sorting(arr):
                 left_counter += 1
             arr_counter += 1
 
-        while left_counter < len(deep_left):
+        while left_counter < len (deep_left):
             arr[arr_counter] = deep_left[left_counter]
             left_counter += 1
             arr_counter += 1
 
-        while right_counter < len(deep_right):
+        while right_counter < len (deep_right):
             arr[arr_counter] = deep_right[right_counter]
             right_counter += 1
             arr_counter += 1
 
-    return arr
+        return arr
+
+
 if __name__ == '__main__' :
     arr = [12,8,5,7,2,8,9,1,2,10]
-    res = sorting(arr)
+    merge = MergeSort()
+    res = merge.sorting(arr)
     print(res)
